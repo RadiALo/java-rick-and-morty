8b6c84c6db40bc78276852827dcfd8409b346a5d
Danilo Kozakov <78644882+RadiALo@users.noreply.github.com>
2023-07-01 18:50:58 +0300

    Initial commit

---
+++ src/main/java/com/radialo/rickandmorty/JavaRickAndMortyApplication.java
@@ -0,0 +1,13 @@
+package com.radialo.rickandmorty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRickAndMortyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaRickAndMortyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/radialo/rickandmorty/JavaRickAndMortyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radialo.rickandmorty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRickAndMortyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
